dd1262383badf184289ce5e52f139b4975512c1b
minanicol18 <mina.chacon@livingstoncollegiate.org>
2025-11-13 18:42:47 +0000

    update

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -42,7 +42,9 @@ drawCircle();
 
 
         // TODO 7 : Use a loop to create multiple circles
-
+for (var i = 0; i < 100; i++) {
+    drawCircle();
+}
 
 
 
@@ -57,24 +59,19 @@ drawCircle();
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-physikz.updatePosition(circles[0]);
-physikz.updatePosition(circles[1]);
-physikz.updatePosition(circles[2]);
-physikz.updatePosition(circles[3]);
-physikz.updatePosition(circles[4]);
+
 
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
 
-game.checkCirclePosition(circles[0]);
-game.checkCirclePosition(circles[1]);
-    game.checkCirclePosition(circles[2]);
-        game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);
+
                
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
+           for (var i = 0; i<circles.length; i++) {
+            physikz.updatePosition(circles[i]);
+            game.checkCirclePosition(circles[i]);
+           }
             
         }
     
@@ -94,13 +91,13 @@ game.checkCirclePosition(circles[1]);
             if(circle.x < 0){
                 circle.x = canvas.width
             }
-if(circle.y < 0){
+             if (circle.y < 0){
 
-    circle.y = canvas.height;
-}
-if(circle.y > canvas.height){
-    circle.y = 0;
-}
+            circle.y = canvas.height;
+            }
+            if (circle.y > canvas.height){
+                circle.y = 0;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
